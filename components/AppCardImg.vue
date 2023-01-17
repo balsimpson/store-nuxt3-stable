a63c740470f09dc7f3e89ba2238bacc97e6689df
balsipson <thirdi@gmail.com>
2023-01-17 12:27:55 +0530

    Cleaned up index page

---
+++ components/AppCardImg.vue
@@ -0,0 +1,35 @@
+<template>
+  <a class="relative overflow-hidden group rounded-xl" :href="'/post/'+item.slug">
+    <div v-if="item.image" class="relative pt-[50%] sm:pt-[70%] rounded-xl overflow-hidden">
+      <img 
+        class="absolute top-0 left-0 object-cover w-full h-full transition-transform duration-500 ease-in-out group-hover:scale-105 rounded-xl"
+        :src="item.image" alt="Image Description">
+      
+        <span
+        class="absolute top-0 right-0 rounded-tr-xl rounded-bl-xl text-xs font-medium bg-gray-800 text-white py-1.5 px-3">
+        Sponsored
+      </span>
+    </div>
+    <img v-else src="https://source.unsplash.com/random" alt=""   class="absolute top-0 left-0 object-cover w-full h-full transition-transform duration-500 ease-in-out group-hover:scale-105 rounded-xl"/>
+
+    <div class="mt-7">
+      <h3 class="text-xl font-semibold text-gray-800 group-hover:text-gray-600">
+        {{ item.title }}
+      </h3>
+      <p class="mt-3 text-gray-800 ">
+        {{ item.description }}
+      </p>
+      <p class="mt-5 inline-flex items-center gap-x-1.5 text-blue-600 decoration-2 group-hover:underline font-medium">
+        Read more
+        <svg class="w-2.5 h-2.5" width="16" height="16" viewBox="0 0 16 16" fill="none">
+          <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
+            stroke="currentColor" stroke-width="2" stroke-linecap="round" />
+        </svg>
+      </p>
+    </div>
+  </a>
+</template>
+
+<script setup>
+const props = defineProps(["item"])
+</script>
---
+++ components/AppImageCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <a class="relative block group" :href="'/post/'+item.slug">
+    <pre>{{ item.slug }}</pre>
+					<div
+						class="flex-shrink-0 relative w-full rounded-xl overflow-hidden h-[350px] before:absolute before:inset-x-0 before:w-full before:h-full before:bg-gradient-to-t before:from-gray-900/[.7] before:z-[1]">
+						<img class="absolute top-0 left-0 object-cover w-full h-full"
+							:src="item.image"
+							:alt="item.title">
+					</div>
+
+					<div class="absolute inset-x-0 top-0 z-10">
+						<div class="flex flex-col h-full p-4 sm:p-6">
+							<!-- Avatar -->
+							<div class="flex items-center">
+								<div class="flex-shrink-0">
+									<img class="h-[2.875rem] w-[2.875rem] border-2 border-white rounded-full"
+										src="https://images.unsplash.com/photo-1669837401587-f9a4cfe3126e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
+										alt="Image Description">
+								</div>
+								<div class="ml-2.5 sm:ml-4">
+									<h4 class="font-semibold text-white">
+										Gloria
+									</h4>
+									<p class="text-xs text-white/[.8]">
+										Jan 09, 2021
+									</p>
+								</div>
+							</div>
+							<!-- End Avatar -->
+						</div>
+					</div>
+
+					<div class="absolute inset-x-0 bottom-0 z-10">
+						<div class="flex flex-col h-full p-4 sm:p-6">
+							<h3 class="text-lg sm:text-3xl font-semibold text-white group-hover:text-white/[.8]">
+								{{item.title}}
+							</h3>
+							<p class="mt-2 text-white/[.8]">
+								{{item.description}}
+							</p>
+						</div>
+					</div>
+				</a>
+</template>
+
+<script setup>
+const props = defineProps(["item"])
+</script>
